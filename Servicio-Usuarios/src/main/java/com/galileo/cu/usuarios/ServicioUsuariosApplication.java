b9f8f50fdb24c0a7f0d62c203ee2da84803fd8a1
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-09 16:18:29 -0400

    0-INIT version-1.1

--- Servicio-Usuarios/src/main/java/com/galileo/cu/usuarios/ServicioUsuariosApplication.java
+++ Servicio-Usuarios/src/main/java/com/galileo/cu/usuarios/ServicioUsuariosApplication.java
@@ -20,7 +20,7 @@ public class ServicioUsuariosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Usuarios V-2024-10-09 09:51");
+		System.out.println("Usuarios V-2024-10-09 16:17");
 	}
 
 }
