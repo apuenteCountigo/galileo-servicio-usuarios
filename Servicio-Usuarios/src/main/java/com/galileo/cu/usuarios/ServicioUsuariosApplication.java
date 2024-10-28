80a56b54b8eebea8788220adb5de122ba91c5cf8
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-28 14:24:35 -0400

    VERSION 1.1.1 AFTER FIXED BUGS V 1.1.0 AFTER QA OK

--- Servicio-Usuarios/src/main/java/com/galileo/cu/usuarios/ServicioUsuariosApplication.java
+++ Servicio-Usuarios/src/main/java/com/galileo/cu/usuarios/ServicioUsuariosApplication.java
@@ -20,7 +20,7 @@ public class ServicioUsuariosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Usuarios V-1.1.0 24-10-19 18:25");
+		System.out.println("Usuarios V-1.1.1 24-10-28 14:24");
 	}
 
 }
