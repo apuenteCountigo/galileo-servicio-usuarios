16bec33044139606bd37c141b878c2228995230f
apuentes+1-at-178934116267 <apuente@soaint.com>
2025-02-13 08:29:55 -0500

    VERSION 1.1.6

--- Servicio-Usuarios/src/main/java/com/galileo/cu/usuarios/ServicioUsuariosApplication.java
+++ Servicio-Usuarios/src/main/java/com/galileo/cu/usuarios/ServicioUsuariosApplication.java
@@ -20,7 +20,7 @@ public class ServicioUsuariosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Usuarios V-1.1.5 25-01-17 10:39");
+		System.out.println("Usuarios V-1.1.6 25-02-13 08:29");
 	}
 
 }
